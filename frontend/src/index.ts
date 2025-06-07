072b4228918c4cad09fa5787c77164f6650bcf1d
joaofavoretti <contas.joaopedro.favoretti@gmail.com>
2025-06-07 18:51:32 -0300

    Adding keys to the environment variables

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,31 +1,299 @@
-import {
-  Viewer,
-  Cartesian3,
-  Math,
-  Terrain,
-  createOsmBuildingsAsync,
-} from "cesium";
+import * as Cesium from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "./css/main.css";
 
-// CesiumJS has a default access token built in but it's not meant for active use.
-// please set your own access token can be found at: https://cesium.com/ion/tokens.
-// Ion.defaultAccessToken = "YOUR TOKEN HERE";
+//Cesium.Ion.defaultAccessToken =
+//  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMTgxNzg3NC01ZmE2LTRhNTUtOWRlOS01ZGMyZWU4NTViMTEiLCJpZCI6MzA4MjgyLCJpYXQiOjE3NDg4MTI4ODJ9.2ynR2Qhw8_bCBZhXiSQR7m2vYhKBSc6HfvnyG5kl9yg";
+if (process.env.ION_KEY) {
+  Cesium.Ion.defaultAccessToken = process.env.ION_KEY;
+}
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Viewer("cesiumContainer", {
-  terrain: Terrain.fromWorldTerrain(),
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  terrain: Cesium.Terrain.fromWorldTerrain(),
+  selectionIndicator: false,
+  infoBox: false,
+  animation: false,
+  timeline: false,
+  homeButton: false,
+  fullscreenButton: false,
+  navigationHelpButton: false,
+  vrButton: false,
 });
 
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-const osmBuildingsTileset = await createOsmBuildingsAsync();
+viewer.cesiumWidget.creditContainer.remove();
+
+const osmBuildingsTileset = await Cesium.createOsmBuildingsAsync();
 viewer.scene.primitives.add(osmBuildingsTileset);
 
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-  orientation: {
-    heading: Math.toRadians(0.0),
-    pitch: Math.toRadians(-15.0),
+const scene = viewer.scene;
+
+viewer.scene.screenSpaceCameraController.zoomEventTypes = [
+  Cesium.CameraEventType.RIGHT_DRAG,
+  Cesium.CameraEventType.WHEEL,
+  Cesium.CameraEventType.PINCH,
+];
+
+function createPoint(worldPosition: any) {
+  const point = viewer.entities.add({
+    position: worldPosition,
+    point: {
+      color: Cesium.Color.RED,
+      pixelSize: 10,
+    },
+  });
+  return point;
+}
+
+viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(
+  Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK,
+);
+
+const handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
+
+let groundPoint: any;
+let groundPosition: any;
+let height: any;
+let radius = 50.0;
+let lineFromGroundToHeaven;
+let floatingPoint: any;
+let floatingLabel: any;
+let cylinder: any;
+let maxCylinder: any;
+let wheelAcceleration = 1.0;
+
+handler.setInputAction(function(movement: any) {
+  if (!Cesium.defined(groundPoint)) {
+    const ray = viewer.camera.getPickRay(movement.position);
+    const earthPosition = viewer.scene.globe.pick(ray, viewer.scene);
+
+    if (!Cesium.defined(earthPosition)) {
+      return;
+    }
+
+    groundPoint = createPoint(earthPosition);
+    floatingPoint = createPoint(earthPosition);
+    groundPosition = earthPosition;
+    //floatingLabel = annotations.add({
+    //  position: groundPosition,
+    //  text: '0 m',
+    //  showBackground: true,
+    //  font: "14px monospace",
+    //  horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+    //  verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+    //  disableDepthTestDistance: Number.POSITIVE_INFINITY,
+    //});
+
+    let earthCartographic = Cesium.Cartographic.fromCartesian(earthPosition);
+    earthCartographic.height += 120.0;
+    const infinityPosition = Cesium.Cartographic.toCartesian(earthCartographic);
+
+    //lineFromGroundToHeaven = viewer.entities.add({
+    //  name: "lineFromGroundToHeaven",
+    //  position: earthPosition,
+    //  polyline: {
+    //    positions: [earthPosition, infinityPosition],
+    //    width: 200,
+    //    // Almost invisible
+    //    material: Cesium.Color.YELLOW.withAlpha(0.7),
+    //    //material: Cesium.Color.YELLOW.withAlpha(0.01),
+    //  },
+    //});
+
+    maxCylinder = viewer.entities.add({
+      position: groundPosition,
+      cylinder: {
+        length: 120.0,
+        topRadius: radius + 1.0,
+        bottomRadius: radius + 1.0,
+        material: Cesium.Color.YELLOW.withAlpha(0.01),
+      },
+    });
+
+    height = 0;
+    cylinder = viewer.entities.add({
+      position: groundPosition,
+      cylinder: {
+        length: height,
+        topRadius: radius,
+        bottomRadius: radius,
+        material: Cesium.Color.RED.withAlpha(0.5),
+        outline: true,
+        outlineColor: Cesium.Color.BLACK,
+      },
+    });
+
+    viewer.scene.screenSpaceCameraController.zoomEventTypes = [
+      Cesium.CameraEventType.RIGHT_DRAG,
+      Cesium.CameraEventType.PINCH,
+    ];
+  } else {
+    if (height === undefined) return;
+
+    viewer.entities.remove(maxCylinder);
+    viewer.entities.remove(groundPoint);
+    viewer.entities.remove(floatingPoint);
+    annotations.remove(floatingLabel);
+
+    radius = 50.0;
+
+    groundPoint = undefined;
+    floatingPoint = undefined;
+    lineFromGroundToHeaven = undefined;
+
+    viewer.scene.screenSpaceCameraController.zoomEventTypes = [
+      Cesium.CameraEventType.RIGHT_DRAG,
+      Cesium.CameraEventType.WHEEL,
+      Cesium.CameraEventType.PINCH,
+    ];
+  }
+}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+handler.setInputAction(function(movement: any) {
+  if (Cesium.defined(floatingPoint)) {
+    const feature = scene.pick(movement.endPosition);
+    if (!Cesium.defined(feature)) return;
+    if (feature.id === undefined || feature.id !== maxCylinder) return;
+
+    const cartesian = scene.pickPosition(movement.endPosition);
+    if (!Cesium.defined(cartesian)) return;
+
+    const cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+    let groundCartographic = Cesium.Cartographic.fromCartesian(groundPosition);
+    height = 2 * (cartographic.height - groundCartographic.height);
+    const heightText = `${height.toFixed(2)} m`;
+
+    annotations.remove(floatingLabel);
+
+    floatingLabel = annotations.add({
+      position: cartesian,
+      text: heightText,
+      showBackground: true,
+      font: "14px monospace",
+      horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+      verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+      disableDepthTestDistance: Number.POSITIVE_INFINITY,
+    });
+
+    viewer.entities.remove(cylinder);
+    cylinder = viewer.entities.add({
+      position: groundPosition,
+      cylinder: {
+        length: height,
+        topRadius: radius,
+        bottomRadius: radius,
+        material: Cesium.Color.RED.withAlpha(0.5),
+        outline: true,
+        outlineColor: Cesium.Color.BLACK,
+      },
+    });
+
+    // viewer.entities.remove(floatingPoint);
+    // floatingPoint = createPoint(
+    floatingPoint.position.setValue(cartesian);
+    // floatingLabel.text = heightText;
+    // floatingLabel.position = cartesian;
+  }
+}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+handler.setInputAction(function(movement: any) {
+  if (!Cesium.defined(groundPoint)) return;
+
+  radius += Math.sign(movement) * wheelAcceleration;
+
+  viewer.entities.remove(maxCylinder);
+  maxCylinder = viewer.entities.add({
+    position: groundPosition,
+    cylinder: {
+      length: 120.0,
+      topRadius: radius + 1.0,
+      bottomRadius: radius + 1.0,
+      material: Cesium.Color.YELLOW.withAlpha(0.01),
+    },
+  });
+
+  viewer.entities.remove(cylinder);
+  cylinder = viewer.entities.add({
+    position: groundPosition,
+    cylinder: {
+      length: height,
+      topRadius: radius,
+      bottomRadius: radius,
+      material: Cesium.Color.RED.withAlpha(0.5),
+      outline: true,
+      outlineColor: Cesium.Color.BLACK,
+    },
+  });
+}, Cesium.ScreenSpaceEventType.WHEEL);
+
+// handler.setInputAction(function (event) {
+//   // We use `viewer.scene.globe.pick here instead of `viewer.camera.pickEllipsoid` so that
+//   // we get the correct point when mousing over terrain.
+//   const ray = viewer.camera.getPickRay(event.position);
+//   const earthPosition = viewer.scene.globe.pick(ray, viewer.scene);
+//   // `earthPosition` will be undefined if our mouse is not over the globe.
+//   if (Cesium.defined(earthPosition)) {
+//
+//     // Create a cilinder
+//     const cylinder = viewer.entities.add({
+//       position: earthPosition,
+// 	cylinder: {
+// 		    length: 400.0,
+// 		    topRadius: 50.0,
+// 		    bottomRadius: 50.0,
+// 		    material: Cesium.Color.RED.withAlpha(0.5),
+// 		    outline: true,
+// 		    outlineColor: Cesium.Color.BLACK,
+// 		},
+// 	  });
+//
+//
+//   }
+// }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+if (!scene.pickPositionSupported) {
+  window.alert("This browser does not support picking positions.");
+}
+
+const greenCylinder = viewer.entities.add({
+  position: Cesium.Cartesian3.fromDegrees(-45.873938, -23.212619, 700.0),
+  cylinder: {
+    length: 200.0,
+    topRadius: 50.0,
+    bottomRadius: 50.0,
+    material: Cesium.Color.GREEN.withAlpha(0.5),
+    outline: true,
+    outlineColor: Cesium.Color.BLACK,
   },
 });
+
+const annotations = scene.primitives.add(new Cesium.LabelCollection());
+
+handler.setInputAction(function(movement: any) {
+  if (!scene.pickPositionSupported) return;
+
+  const feature = scene.pick(movement.position);
+  if (!Cesium.defined(feature)) return;
+
+  const cartesian = scene.pickPosition(movement.position);
+  if (!Cesium.defined(cartesian)) return;
+
+  const cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+  const height = `${cartographic.height.toFixed(2)} m`;
+
+  annotations.add({
+    position: cartesian,
+    text: height,
+    showBackground: true,
+    font: "14px monospace",
+    horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+    verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+    disableDepthTestDistance: Number.POSITIVE_INFINITY,
+  });
+}, Cesium.ScreenSpaceEventType.RIGHT_CLICK);
+
+viewer.camera.lookAt(
+  Cesium.Cartesian3.fromDegrees(-45.873938, -23.212619, 200.0),
+  new Cesium.Cartesian3(800.0, 800.0, 800.0),
+);
+viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,74 +4,82 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 const cesiumSource = "node_modules/cesium/Build/Cesium";
 const cesiumBaseUrl = "cesiumStatic";
 
-module.exports = {
-	entry: "./src/index.ts",
-	devtool: "inline-source-map",
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-				type: "asset/inline",
-			},
-		],
-	},
-	resolve: {
-		mainFiles: ["index", "Cesium"],
-		extensions: [".tsx", ".ts", ".js"],
-	},
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
-	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "dist"),
-		},
-		liveReload: true,
-		port: 9000,
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "src/index.html",
-		}),
-		new CopyWebpackPlugin({
-			patterns: [
+module.exports = (env) => {
+	return {
+		entry: "./src/index.ts",
+		devtool: "inline-source-map",
+		module: {
+			rules: [
 				{
-					from: path.join(cesiumSource, "Workers"),
-					to: `${cesiumBaseUrl}/Workers`,
+					test: /\.tsx?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
 				},
 				{
-					from: path.join(cesiumSource, "ThirdParty"),
-					to: `${cesiumBaseUrl}/ThirdParty`,
+					test: /\.css$/,
+					use: ["style-loader", "css-loader"],
 				},
 				{
-					from: path.join(cesiumSource, "Assets"),
-					to: `${cesiumBaseUrl}/Assets`,
-				},
-				{
-					from: path.join(cesiumSource, "Widgets"),
-					to: `${cesiumBaseUrl}/Widgets`,
+					test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+					type: "asset/inline",
 				},
 			],
-		}),
-		new webpack.DefinePlugin({
-			// Define relative base path in cesium for loading assets
-			CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
-		}),
-	],
-	mode: "development",
-	devtool: "eval",
+		},
+		resolve: {
+			mainFiles: ["index", "Cesium"],
+			extensions: [".tsx", ".ts", ".js"],
+		},
+		output: {
+			filename: "bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		devServer: {
+			static: {
+				directory: path.join(__dirname, "dist"),
+			},
+			liveReload: true,
+			port: 9000,
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "src/index.html",
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
+					{
+						from: path.join(cesiumSource, "Workers"),
+						to: `${cesiumBaseUrl}/Workers`,
+					},
+					{
+						from: path.join(cesiumSource, "ThirdParty"),
+						to: `${cesiumBaseUrl}/ThirdParty`,
+					},
+					{
+						from: path.join(cesiumSource, "Assets"),
+						to: `${cesiumBaseUrl}/Assets`,
+					},
+					{
+						from: path.join(cesiumSource, "Widgets"),
+						to: `${cesiumBaseUrl}/Widgets`,
+					},
+				],
+			}),
+			new webpack.DefinePlugin({
+				// Define relative base path in cesium for loading assets
+				CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
+				"process.env.ENVIRONMENT": JSON.stringify("development"),
+			}),
+			new Dotenv({
+				path: env.environment === 'prod' ? path.resolve(__dirname, ".env.prod") : path.resolve(__dirname, ".env.dev"),
+				safe: true,
+			}),
+		],
+		mode: "development",
+		devtool: "eval",
+	};
 };
